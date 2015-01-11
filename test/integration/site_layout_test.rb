6661c5821d1fb7297926b737be03e2627b00a58b
Nicholas DiMucci <ndimucci84@gmail.com>
2015-01-11 22:45:34 +0000

    improved tests for title

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "layout_links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +12,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+  
+  test "signup_title" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")
   end
 end
